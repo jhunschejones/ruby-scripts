78c58d8f43d17b584e5b5950272224ac1e34901e
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-07-08 14:34:54 -0700

    Refactor CSV printing
    - Sort expenses by vendor before outputing them
    - Collapse a few code blocks for easier readability

--- catagorize/catagorize.rb
+++ catagorize/catagorize.rb
@@ -15,50 +15,32 @@ CSV.foreach("./expenses.csv") do |row|
     )
   end
 end
-puts "" # add newline after debug progress
 
-CSV.open("./parsed_expenses.csv", "wb") do |csv|
-  csv << [
-    "Transaction Date",
-    "Description",
-    "Amount",
-    "Category"
-  ]
+expenses_by_vendor = expenses.group_by(&:vendor).map do |vendor, expenses|
+  OpenStruct.new(
+    vendor: vendor,
+    total: expenses.map { |expense| expense.amount.to_f }.reduce(0, :+).abs
+  )
+end.sort_by(&:total).reverse
+
+puts "" # add newline after parsing progress dots
+
+CSV.open("./parsed_expenses/parsed_expenses.csv", "wb") do |csv|
+  csv << ["Transaction Date", "Description", "Amount", "Category"]
   expenses.each do |expense|
-    csv << [
-      expense.transaction_date,
-      expense.description,
-      expense.amount,
-      expense.category
-    ]
+    csv << [expense.transaction_date, expense.description, expense.amount, expense.category]
   end
 end
 
-CSV.open("./expenses_by_vendors.csv", "wb") do |csv|
-  csv << [
-    "Vendor",
-    "Total"
-  ]
-  expenses.group_by(&:vendor).each do |vendor, expenses|
-    csv << [
-      vendor,
-      expenses.map { |expense| expense.amount.to_f }.reduce(0, :+).abs
-    ]
+CSV.open("./parsed_expenses/expenses_by_vendor.csv", "wb") do |csv|
+  csv << ["Vendor", "Total" ]
+  expenses_by_vendor.each do |expense_by_vendor|
+    csv << [expense_by_vendor[:vendor], expense_by_vendor[:total]]
   end
 end
 
 # Print out the unknown vendors for debugging purposes
-CSV.open("./unknown_vendors.csv", "wb") do |csv|
-  csv << [
-    "Description",
-    "Total"
-  ]
-  expenses.each do |expense|
-    if expense.vendor == "Unknown"
-      csv << [
-        expense.description,
-        expense.amount
-      ]
-    end
-  end
+CSV.open("./parsed_expenses/unknown_vendors.csv", "wb") do |csv|
+  csv << ["Description", "Total"]
+  expenses.each { |expense| csv << [expense.description, expense.amount] if expense.vendor == "Unknown" }
 end
--- catagorize/lib/module_loader.rb
+++ catagorize/lib/module_loader.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'yaml'
+require 'ostruct'
 
 Dir["#{File.dirname(__FILE__)}/*.rb"].each { |f| require(f) }
 Dir["#{File.dirname(__FILE__)}/**/*.rb"].each { |f| require(f) }
