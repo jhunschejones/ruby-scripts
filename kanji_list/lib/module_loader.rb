8fed9c881fd22e17c56a53e4e31db2c3f89f6190
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-10-28 12:26:23 -0700

    Log active record SQL queries when running in console mode

--- kanji_list/lib/module_loader.rb
+++ kanji_list/lib/module_loader.rb
@@ -6,4 +6,5 @@ require_relative '../db/connection'
 Dir["#{File.dirname(__FILE__)}/*.rb"].each { |f| require(f) }
 Dir["#{File.dirname(__FILE__)}/**/*.rb"].each { |f| require(f) }
 
+ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV["LOG_QUERIES"]
 $logger = Logger.new('tmp/log.txt')
