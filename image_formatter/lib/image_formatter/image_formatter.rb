ecaf7e2ec69f52936e20a4d5c44500be79186fba
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-10-30 15:06:41 -0700

    Create temp file while tinyification is in process, handle errors more gracefully

--- image_formatter/lib/image_formatter/image_formatter.rb
+++ image_formatter/lib/image_formatter/image_formatter.rb
@@ -2,6 +2,7 @@ class ImageFormatter
   TARGET_HEIGHT_PX = 480
   TARGET_FILESIZE_KB = 50
   PROCESSED_IMAGE_SUFFIX = "_tinyified".freeze
+  TEMP_FILE_EXTENSION = ".tinyifying".freeze
   SUPPORTED_EXTENSIONS = [".jpeg", ".jpg", ".png"].freeze
 
   attr_reader :filename, :event
@@ -12,27 +13,38 @@ class ImageFormatter
   end
 
   def process_event
-    image_file = File.open(filename, 'rb')
+    image_file = File.open(filename, "rb")
     filesize_kb = (image_file.size.to_f / 1000).to_i
     height_px = ImageSize.new(image_file).height
 
     if filesize_kb > TARGET_FILESIZE_KB || height_px > TARGET_HEIGHT_PX
-      log("\e[33m====== Tinyifying #{filename} ======\033[0m")
+      log("====== Tinyifying #{filename} ======".yellow)
 
-      Tinify.from_file(filename)
-            .resize(method: "scale", height: TARGET_HEIGHT_PX)
-            .to_file(output_file_name)
+      begin
+        create_temp_file
 
-      FileUtils.mv(filename, BACKUP_FILES_PATH)
+        Tinify
+          .from_file(filename)
+          .resize(method: "scale", height: TARGET_HEIGHT_PX)
+          .to_file(output_file_name)
 
-      puts "\e[32m====== Generated #{output_file_name} ======\033[0m"
+        log("====== Generated #{output_file_name} ======".green)
+        put_file_in_backup_directory
+      rescue => e
+        log("Unable to tinyify #{filename}: #{e.message}".red)
+      end
+
+      clean_up_temp_file
     else
       puts "Skipping #{event} event for #{filename}: filesize is already small enough"
     end
   end
 
   def should_process_event?
-    event == :created && !filename.include?(PROCESSED_IMAGE_SUFFIX) && SUPPORTED_EXTENSIONS.include?(File.extname(filename))
+    event == :created &&
+      !filename.include?(PROCESSED_IMAGE_SUFFIX) &&
+      !filename.include?(TEMP_FILE_EXTENSION) &&
+      SUPPORTED_EXTENSIONS.include?(File.extname(filename))
   end
 
   private
@@ -43,5 +55,24 @@ class ImageFormatter
     extension = File.extname(filename)
 
     "#{directory_path}/#{base_filename}#{PROCESSED_IMAGE_SUFFIX}#{extension}"
+  end
+
+  def temp_file_name
+    directory_path = File.dirname(filename)
+    base_filename = File.basename(filename, File.extname(filename))
+
+    "#{directory_path}/#{base_filename}#{TEMP_FILE_EXTENSION}"
+  end
+
+  def create_temp_file
+    File.new(temp_file_name, "w")
+  end
+
+  def clean_up_temp_file
+    FileUtils.rm(temp_file_name)
+  end
+
+  def put_file_in_backup_directory
+    FileUtils.mv(filename, BACKUP_FILES_PATH)
   end
 end
