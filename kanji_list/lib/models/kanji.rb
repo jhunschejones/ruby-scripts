150d98351dcc3a30fb46e66c0b150e6f486b184d
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-11-28 20:39:19 -0800

    Use constants for file paths (+ code cleanup)

--- kanji_list/lib/models/kanji.rb
+++ kanji_list/lib/models/kanji.rb
@@ -37,7 +37,7 @@ class Kanji < ActiveRecord::Base
 
     def remaining_characters
       previous_characters = Kanji.pluck(:character)
-      new_characters = YAML::load(File.open('config/word_list.yml'))["new_words"]
+      new_characters = YAML::load(File.open(WORD_LIST_YAML_PATH))[WORD_LIST_KEY]
         .flat_map { |word| word.split("") }
         .uniq
         .select { |kanji| kanji =~ KANJI_REGEX }
@@ -46,7 +46,7 @@ class Kanji < ActiveRecord::Base
     end
 
     def dump_to_yaml
-      File.open("db/kanji_list_dump.yml", "w") do |file|
+      File.open(KANJI_YAML_DUMP_PATH, "w") do |file|
         file.write(
           {
             "added_kanji" => Kanji.where(status: ADDED_STATUS).pluck(:character),
@@ -57,7 +57,7 @@ class Kanji < ActiveRecord::Base
     end
 
     def load_from_yaml_dump
-      dump = YAML::load(File.open("db/kanji_list_dump.yml"))
+      dump = YAML::load(File.open(KANJI_YAML_DUMP_PATH))
       Kanji.destroy_all
       dump["added_kanji"].each { |kanji| p Kanji.add(kanji) }
       dump["skipped_kanji"].each { |kanji| p Kanji.skip(kanji) }
--- kanji_list/lib/module_loader.rb
+++ kanji_list/lib/module_loader.rb
@@ -1,11 +1,15 @@
-require 'yaml'
-require 'logger'
-require 'tty-prompt'
-require 'active_record'
-require_relative '../db/connection'
+require "yaml"
+require "logger"
+require "tty-prompt"
+require "active_record"
+require_relative "../db/connection"
 
 Dir["#{File.dirname(__FILE__)}/*.rb"].each { |f| require(f) }
 Dir["#{File.dirname(__FILE__)}/**/*.rb"].each { |f| require(f) }
 
 ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV["LOG_QUERIES"]
-$logger = Logger.new('tmp/log.txt')
+$logger = Logger.new("tmp/log.txt")
+
+WORD_LIST_YAML_PATH = "config/word_list.yml".freeze
+WORD_LIST_KEY = "new_words".freeze
+KANJI_YAML_DUMP_PATH = "db/kanji_list_dump.yml".freeze
