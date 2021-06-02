d6290c8266877607cb1ceb5a5277cd8f3c200ff9
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-06-02 01:53:40 -0700

    Add option to vlc_dual_audio script for audio delay

--- vlc_dual_audio/lib/cli.rb
+++ vlc_dual_audio/lib/cli.rb
@@ -11,26 +11,31 @@ end
 
 system("clear")
 
-puts "\n====== Lets watch a movie with dual audio! ======".cyan
+puts "====== Lets watch a movie with dual audio! ======".cyan
 puts "(Follow the on-screen instructions and press ENTER to advance through the steps)\n".gray
 
 puts "First, drag and drop the movie file you want to play:"
 print "> #{SET_CLI_TO_GREEN}"
 movie_file = $stdin.gets.chomp.strip
+print RESET_CLI_COLOR
 unless File.file?(movie_file.gsub("\\", ""))
   puts "Hmm, something doesn't look right about that movie file: '#{movie_file}'".red
   exit 1
 end
 
-puts RESET_CLI_COLOR
 puts "Now drag and drop the additional audio file:"
 print "> #{SET_CLI_TO_GREEN}"
 audio_file = $stdin.gets.chomp.strip
+print RESET_CLI_COLOR
 unless File.file?(audio_file.gsub("\\", ""))
   puts "Hmm, something doesn't look right about that audio file: '#{audio_file}'".red
   exit 1
 end
 
-puts RESET_CLI_COLOR
+puts "Optional: provide a number of milliseconds to delay the audio file, or press ENTER to skip:"
+print "> #{SET_CLI_TO_GREEN}"
+user_specified_delay_ms = $stdin.gets.chomp.strip
+print RESET_CLI_COLOR
+audio_delay_ms = user_specified_delay_ms.empty? ? 0 : user_specified_delay_ms
 
-system("vlc #{movie_file} --input-slave #{audio_file} #{LAST_AUDIO_TRACK} #{DISABLE_SUBTITLES}")
+system("vlc #{movie_file} --input-slave #{audio_file} #{LAST_AUDIO_TRACK} #{DISABLE_SUBTITLES} --audio-desync=#{audio_delay_ms}")
