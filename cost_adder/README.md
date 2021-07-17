7b23deab808c97d19a09404aa1ac88143879c646
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-07-16 17:31:20 -0700

    Add cost_adder script

---
+++ cost_adder/README.md
@@ -0,0 +1,6 @@
+# Cost Adder
+
+The Cost Adder script reads dollar values out of a blob of text and sums the total of those amounts. To find the values, it looks for string matches that conform to the `$0.00` USD format.
+
+### In use
+To use the script, simply place the text you wish to parse in `./costs_to_add.txt` and then call `./bin/run` to execute.
---
+++ cost_adder/lib/cost_adder.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "money"
+require "monetize"
+
+Money.default_currency = "USD"
+Money.rounding_mode = 1
+Money.locale_backend = :currency
+
+DOLLAR_LOOKING_NUMBERS_ANYWHERE_REGEX = /(\d+\,?\d+?\.?\d{2}?)/
+TRAILING_DOLLAR_NUMBERS_REGEX = /\$\S+/
+
+TEXT_TO_PARSE_PATH = "costs_to_add.txt"
+
+text_to_parse = File.read(TEXT_TO_PARSE_PATH)
+
+total_cents = text_to_parse
+  .scan(TRAILING_DOLLAR_NUMBERS_REGEX)
+  .map{ |price| Monetize.parse(price).cents }
+  .reduce(0, :+)
+
+puts "Total: #{Money.new(total_cents, "USD").format}"
