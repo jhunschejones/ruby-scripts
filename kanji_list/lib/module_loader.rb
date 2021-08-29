02b5926dab67810ae4ec13652f2ae25dc803ac28
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-08-29 13:41:23 -0700

    Separate out test environment

--- kanji_list/lib/module_loader.rb
+++ kanji_list/lib/module_loader.rb
@@ -12,9 +12,18 @@ end
 ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV["LOG_QUERIES"]
 $logger = Logger.new("tmp/log.txt")
 
-WORD_LIST_YAML_PATH = "config/word_list.yml".freeze
 WORD_LIST_KEY = "new_words".freeze
-KANJI_YAML_DUMP_PATH = "db/kanji_list_dump.yml".freeze
-AWS_BUCKET = "kanji-list".freeze
+
+if ENV["SCRIPT_ENV"] == "development"
+  WORD_LIST_YAML_PATH = "config/word_list.yml".freeze
+  KANJI_YAML_DUMP_PATH = "db/kanji_list_dump.yml".freeze
+  AWS_BUCKET = "kanji-list".freeze
+elsif ENV["SCRIPT_ENV"] == "test"
+  WORD_LIST_YAML_PATH = "config/test_word_list.yml".freeze
+  KANJI_YAML_DUMP_PATH = "db/test_kanji_list_dump.yml".freeze
+  AWS_BUCKET = nil # Don't mess with AWS backups in test mode
+else
+  raise "Unrecognized environment '#{ENV["SCRIPT_ENV"]}'"
+end
 
 File.write(WORD_LIST_YAML_PATH, "#{WORD_LIST_KEY}: []") unless File.exist?(WORD_LIST_YAML_PATH)
