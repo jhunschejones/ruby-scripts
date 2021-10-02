c3dfcfd7ef71247477d6387bb315eb3947d39a26
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-10-01 17:45:14 -0700

    Finish hooking up to pCloud

--- kanji_list/lib/main.rb
+++ kanji_list/lib/main.rb
@@ -1,42 +1,9 @@
 require_relative './module_loader'
 
-db_file_name = YAML::load(File.open("config/database.yml")).fetch(ENV["SCRIPT_ENV"])["database"]
-
-# Always load state from pcloud when starting
-
-# ::File.open(db_file_name, "w") do |file|
-#   file.binmode
-#   print "Saving your file.."
-#   HTTParty.get(download_url, stream_body: true) do |fragment|
-#     file.write(fragment)
-#     print "."
-#   end
-#   puts "."
-# end
-
 puts "=======  Welcome to Kanji List!  =======".cyan
-
-kanji_list_dump_pcloud_file_id = ENV["KANJI_LIST_DUMP_PCLOUD_FILE_ID"].to_i
-kanji_db_pcloud_file_id = ENV["KANJI_DB_PCLOUD_FILE_ID"].to_i
-kanji_list_pcloud_folder_id = ENV["KANJI_LIST_PCLOUD_FOLDER_ID"].to_i
 
 begin
   CLI.new.run
 rescue CLI::ManualInterrupt
-  # Always backup when quitting
-  Rake::Task["db:dump_to_yaml"].invoke
-
-  Pcloud::File.upload(
-    folder_id: kanji_list_pcloud_folder_id,
-    filename: KANJI_YAML_DUMP_PATH.split("/").last,
-    file: File.open("./#{KANJI_YAML_DUMP_PATH}"),
-    overwrite: true
-  )
-
-  Pcloud::File.upload(
-    folder_id: kanji_list_pcloud_folder_id,
-    filename: db_file_name,
-    file: File.open("./#{db_file_name}"),
-    overwrite: true
-  )
+  Rake::Task["db:upload_to_pcloud"].invoke unless ENV["SCRIPT_ENV"] == "test"
 end
--- kanji_list/lib/module_loader.rb
+++ kanji_list/lib/module_loader.rb
@@ -6,36 +6,38 @@ require "active_record"
 require "aws-sdk-s3"
 require "aws-sdk-sns"
 require "pcloud_api"
-require_relative "../db/connection"
 
 Dir["#{File.dirname(__FILE__)}/**/*.rb"].each do |file|
   require(file) unless File.basename(file) == "main.rb"
 end
 
-ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV["LOG_QUERIES"]
-$logger = Logger.new("tmp/log.txt") unless ENV["SCRIPT_ENV"] == "test"
-
 WORD_LIST_KEY = "new_words".freeze
+LOCAL_DB_FILENAME = YAML::load(File.open("config/database.yml")).fetch(ENV["SCRIPT_ENV"])["database"]
 
 if ENV["SCRIPT_ENV"] == "development"
   WORD_LIST_YAML_PATH = "config/word_list.yml".freeze
   KANJI_YAML_DUMP_PATH = "db/kanji_list_dump.yml".freeze
   AWS_BUCKET = "kanji-list".freeze
+  KANJI_LIST_PCLOUD_FOLDER_ID = ENV["KANJI_LIST_PCLOUD_FOLDER_ID"].to_i
+  KANJI_LIST_PCLOUD_ARCHIVE_FOLDER_ID = ENV["KANJI_LIST_PCLOUD_ARCHIVE_FOLDER_ID"].to_i
 elsif ENV["SCRIPT_ENV"] == "test"
   WORD_LIST_YAML_PATH = "config/test_word_list.yml".freeze
   KANJI_YAML_DUMP_PATH = "db/test_kanji_list_dump.yml".freeze
   AWS_BUCKET = "test-kanji-list" # Doesn't actually exist
+  KANJI_LIST_PCLOUD_FOLDER_ID = nil # Doesn't exist
+  KANJI_LIST_PCLOUD_ARCHIVE_FOLDER_ID = nil # Doesn't exist
 else
   raise "Unrecognized environment '#{ENV["SCRIPT_ENV"]}'"
 end
 
-# Pcloud::Client.configure(
-# 	access_token: ENV["PCLOUD_API_ACCESS_TOKEN"],
-# 	data_region: "EU"
-# )
-
-File.write(WORD_LIST_YAML_PATH, "#{WORD_LIST_KEY}: []") unless File.exist?(WORD_LIST_YAML_PATH)
-
-# Make the rake tasks availibile for the CLI and for unit tests
+# Make rake tasks availibile for the CLI and unit tests
 load "lib/tasks/db.rake"
 Rake::Task.define_task(:environment)
+
+# Load state from pcloud before connecting to the database
+Rake::Task["db:download_from_pcloud"].invoke unless ENV["SCRIPT_ENV"] == "test"
+require_relative "../db/connection"
+ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV["LOG_QUERIES"]
+$logger = Logger.new("tmp/log.txt") unless ENV["SCRIPT_ENV"] == "test"
+
+File.write(WORD_LIST_YAML_PATH, "#{WORD_LIST_KEY}: []") unless File.exist?(WORD_LIST_YAML_PATH)
