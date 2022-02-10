8c4099cd7794ef2b4514be4529c9cb2d1dd88b54
Joshua Hunsche Jones <jhunschejones@github.com>
2022-02-10 09:55:20 -0600

    Update Rails + AWS deps

--- kanji_list/db/schema.rb
+++ kanji_list/db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
+ActiveRecord::Schema[7.0].define(version: 1) do
   create_table "kanjis", force: :cascade do |t|
     t.string "character", null: false
     t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["character"], name: "index_kanjis_on_character", unique: true
   end
 
