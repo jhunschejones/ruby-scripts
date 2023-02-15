9a116fd5f54e9a0d6ff26f519af2faf8b0ee7dd8
Joshua Hunsche Jones <jhunschejones@github.com>
2023-02-15 17:07:45 -0600

    Update script to locally parse out study time

--- parse_jpdb_stats/README.md
+++ parse_jpdb_stats/README.md
@@ -1,9 +1,11 @@
 # Parse JPDB Stats
 
 ### Overview
-There are already tools in place for gathering and creating daily study time stats for jpdb.io. This set of scripts simply cobbles those tools together to speed up the process of getting my daily study time for a specific date.
+There are already several tools in place for gathering and creating daily study time stats for jpdb.io. This set of scripts helps cobble these tools together to speed up the process of getting my daily study time on jpdb.io.
+
+Special thanks to @bijak for his help building the logic for estimating number of minutes spent reviewing per day 🫶🏻✨
 
 ### Usage
-The `./bin/run` coordinates the three pieces of this tool, downloading the `reviews.json` from jpdb.io, using it to generate stats from https://github.com/bijak/jpdb_stats, then parsing the stats out to some simple command line output showing the total study minutes per day.
+The `./bin/run` script coordinates all the moving parts of this tool, downloading the `reviews.json` from jpdb.io, parsing the stats out to some simple command line output showing the total study minutes per day, then logging your script session out of jpdb.io.
 
-NOTE: You will need the 1Password CLI installed and configured for the scripts to work out of the box, as it is used in `./bin/download-jpdb-reviews`. If you wish to use another tool to safely read out your username and password you may modifiy this file accordingly.
+NOTE: You will need the 1Password CLI installed and configured for the scripts to work out of the box, as it is used in `./bin/download-jpdb-reviews`. If you wish to use another tool to safely read out your username and password you may modify this file accordingly.
---
+++ parse_jpdb_stats/local_parse.rb
@@ -0,0 +1,70 @@
+# ./local_parse.rb explanation
+#
+# Required state:
+# `./tmp/reviews.json`: the exported reviews JSON from your jpdb.io account
+#
+# Output state:
+# `./tmp/daily_study_time.txt`: daily review minutes (also printed to standard out)
+
+require "json"
+require "date"
+
+# Some styled CLI output options
+class String
+  def red;     "\e[31m#{self}\e[0m" end
+  def green;   "\e[32m#{self}\e[0m" end
+  def black;   "\e[30m#{self}\e[0m" end
+  def magenta; "\e[35m#{self}\e[0m" end
+  def brown;   "\e[33m#{self}\e[0m" end
+  def blue;    "\e[34m#{self}\e[0m" end
+  def cyan;    "\e[36m#{self}\e[0m" end
+  def gray;    "\e[37m#{self}\e[0m" end
+  def bold;    "\e[1m#{self}\e[22m" end
+end
+
+DAILY_STUDY_TIME_RELATIVE_PATH = "./tmp/daily_study_time.txt".freeze
+REVIEWS_RELATIVE_PATH = "./tmp/reviews.json".freeze
+
+reviews_file_path = File.expand_path(REVIEWS_RELATIVE_PATH)
+raise "Missing #{reviews_file_path}".red unless File.exists?(reviews_file_path)
+reviews_file = File.read(reviews_file_path)
+reviews_json = JSON.parse(reviews_file)
+
+review_categories = reviews_json.keys
+review_timestamps = review_categories.flat_map do |review_category|
+  reviews_json[review_category].flat_map { |entry| entry["reviews"].map { |review| review["timestamp"] } }
+end
+
+review_minutes_per_day = {}
+
+review_timestamps.uniq.map do |timestamp|
+  rounded_time = Time.at(timestamp - (timestamp % 60))
+  date = rounded_time.to_s.split.first
+
+  if review_minutes_per_day[date]
+    review_minutes_per_day[date] << rounded_time.to_i
+  else
+    review_minutes_per_day[date] = [rounded_time.to_i]
+  end
+end
+
+review_days = review_minutes_per_day.keys.sort.reverse
+daily_study_time_entries = []
+
+puts "Here are your jpdb.io daily study time stats:".cyan
+review_days.each do |review_day|
+  minutes_entries = review_minutes_per_day[review_day].uniq.size
+  daily_study_time_entries << "Date: #{review_day}, Total minutes: #{minutes_entries}"
+  puts "#{"Date:".gray} #{review_day}#{", Total minutes:".gray} #{minutes_entries.to_s.bold}"
+end
+
+File.open(DAILY_STUDY_TIME_RELATIVE_PATH, "w+") do |f|
+  f.puts("Daily jpdb.io study time generated on: #{Date.today}")
+  f.puts("=================================================")
+  f.puts(daily_study_time_entries)
+end
+
+if ENV["KEEP_DAILY_STUDY_TIME"]
+  # Optionally copy the file into the project directory to be commited
+  FileUtils.cp(DAILY_STUDY_TIME_RELATIVE_PATH, "./daily_study_time.txt")
+end
