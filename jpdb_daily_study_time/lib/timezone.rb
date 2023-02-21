17d1085fc3bc082e0d3fd8b3aaf1413331dc6fc8
Joshua Hunsche Jones <jhunschejones@github.com>
2023-02-21 11:23:24 -0600

    Add timezone debug logs

--- jpdb_daily_study_time/lib/timezone.rb
+++ jpdb_daily_study_time/lib/timezone.rb
@@ -3,6 +3,7 @@ require "tzinfo"
 module Timezone
   def self.for_seconds_timestamp(timestamp)
     timezone_name =
+      # Start of March 2 to start of March 9, 2023
       if timestamp > 1677736800 && timestamp < 1678341600
         "America/New_York"
       else
