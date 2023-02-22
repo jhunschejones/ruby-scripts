a0a36a467786a4ae0bf9b57c190d9f3055280074
Joshua Hunsche Jones <jhunschejones@github.com>
2023-02-22 09:46:44 -0600

    Fix timestamps to be UTC+5

--- jpdb_daily_study_time/lib/timezone.rb
+++ jpdb_daily_study_time/lib/timezone.rb
@@ -3,8 +3,8 @@ require "tzinfo"
 module Timezone
   def self.for_seconds_timestamp(timestamp)
     timezone_name =
-      # Start of March 2 to start of March 9, 2023
-      if timestamp > 1677736800 && timestamp < 1678341600
+      # March 2 to March 9, 2023 trip to the east coast
+      if timestamp > 1677754800 && timestamp < 1678359600
         "America/New_York"
       else
         "America/Chicago"
