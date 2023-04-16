ba5f95a0b21f012e94e49484493aff9bf106c005
jhunschejones <joshjones103@gmail.com>
2023-04-16 12:19:08 -0500

    Add LOCAL_FOLDER_PATH and ability to download files

--- pcloud_folder_sync/lib/main.rb
+++ pcloud_folder_sync/lib/main.rb
@@ -1,9 +1,27 @@
 require "pcloud_api"
 
+raise "Couldn't find '#{ENV["LOCAL_FOLDER_PATH"]}' directory" unless Dir.exist?(ENV["LOCAL_FOLDER_PATH"])
+
 Pcloud::Client.configure(
   access_token: ENV["PCLOUD_ACCESS_TOKEN"],
   data_region: "EU"
 )
 
-folder = Pcloud::Folder.find_by(path: ENV["PCLOUD_FOLDER_PATH"])
-puts "Found #{folder.contents.size} items in your folder"
+Pcloud::Folder
+  .find_by(path: ENV["PCLOUD_FOLDER_PATH"])
+  .contents
+  .filter { |item| item.is_a?(Pcloud::File) }
+  .each do |pcloud_file|
+    local_file = ENV["LOCAL_FOLDER_PATH"] + "/" + pcloud_file.name
+
+    # Local file already exists and is newer than the version in pCloud
+    next if ::File.exist?(local_file) && ::File.ctime(local_file) > pcloud_file.created_at
+
+    ::File.open(local_file, "w") do |new_file|
+      new_file.binmode
+      puts "Downloading #{pcloud_file.name} from pCloud..."
+      HTTParty.get(pcloud_file.download_url, stream_body: true) do |fragment|
+        new_file.write(fragment)
+      end
+    end
+  end
