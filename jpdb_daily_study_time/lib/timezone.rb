3d9c73a249ea0d0873837513bba5f3a803a4914b
jhunschejones <joshjones103@gmail.com>
2023-05-22 14:14:59 -0500

    Add another east coast trip

--- jpdb_daily_study_time/lib/timezone.rb
+++ jpdb_daily_study_time/lib/timezone.rb
@@ -6,6 +6,9 @@ module Timezone
       # March 2 to March 9, 2023 trip to the east coast
       if timestamp > 1677754800 && timestamp < 1678359600
         "America/New_York"
+      # May 10 to May 16, 2023 trip to the east coast
+      elsif timestamp > 16837128000 && timestamp < 16842816000
+        "America/New_York"
       else
         "America/Chicago"
       end
