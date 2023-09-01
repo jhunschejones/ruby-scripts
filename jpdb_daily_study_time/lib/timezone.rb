cd9297dd25c0633ff7a6695484ec1906843e0c78
Joshua Hunsche Jones <jhunschejones@github.com>
2023-09-01 11:49:02 -0500

    Code simplification and naming cleanup

--- jpdb_daily_study_time/lib/timezone.rb
+++ jpdb_daily_study_time/lib/timezone.rb
@@ -3,30 +3,31 @@ require "time"
 require "yaml"
 
 module Timezone
-  TimezoneChange = Struct.new(:start_date, :end_date, :timezone, keyword_init: true)
+  TimezoneChange = Struct.new(:start_date, :end_date, :timezone_name, keyword_init: true)
+
+  DEFAULT_TIMEZONE = "America/Chicago"
 
   TIMEZONE_CHANGES_FILEPATH = "config/timzeone_changes.yml"
-  TIMEZONE_CHANGES =
+  UNCOMPLETED_TIMEZONE_CHANGES, COMPLETED_TIMEZONE_CHANGES =
     if File.exist?(TIMEZONE_CHANGES_FILEPATH)
       YAML.load(File.read(TIMEZONE_CHANGES_FILEPATH), permitted_classes: [Time])
         .fetch("timezone_changes", [])
-        .map { |tzc| TimezoneChange.new(start_date: tzc["start_date"], end_date: tzc["end_date"], timezone: tzc["timezone"])}
+        .map { |tzc| TimezoneChange.new(start_date: tzc["start_date"], end_date: tzc["end_date"], timezone_name: tzc["timezone_name"]) }
+        .sort_by(&:start_date) # sorting allows us to exit after the first matching timezone_change
+        .partition { |tzc| tzc.end_date.nil? }
     else
-      []
+      [[], []]
     end
-  DEFAULT_TIMEZONE = "America/Chicago"
 
   # `timestamp` is a 10-digit integer for seconds since unix epoch
   def self.for_seconds_timestamp(timestamp)
     time_from_timestamp = Time.at(timestamp)
-    sorted_timezone_changes = TIMEZONE_CHANGES.sort_by(&:start_date)
-    uncompleted_timezone_changes, completed_timezone_changes = sorted_timezone_changes.partition { |tzc| tzc.end_date.nil? }
 
     timezone_name = nil
-    completed_timezone_changes.each do |timezone_change|
+    COMPLETED_TIMEZONE_CHANGES.each do |timezone_change|
       if time_from_timestamp > timezone_change.start_date && time_from_timestamp < timezone_change.end_date
         # timestamp falls in the middle of a completed timezone_change
-        timezone_name = timezone_change.timezone
+        timezone_name = timezone_change.timezone_name
         break
       end
     end
@@ -34,10 +35,10 @@ module Timezone
     # if timezone_name is still nil, that means it does not fall in the middle of a completed timezone change
     # so we want to try and see if there is an uncompleted timezone change that might apply
     if timezone_name.nil?
-      uncompleted_timezone_changes.each do |timezone_change|
+      UNCOMPLETED_TIMEZONE_CHANGES.each do |timezone_change|
         if time_from_timestamp > timezone_change.start_date
           # timestamp falls inside an uncompleted / currently active timezone_change
-          timezone_name = timezone_change.timezone
+          timezone_name = timezone_change.timezone_name
           break
         end
       end
