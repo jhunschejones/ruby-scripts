dcd2a0e4231fea47ac72285af522b70169d010c8
Joshua Hunsche Jones <jhunschejones@github.com>
2023-09-01 11:38:14 -0500

    Use a config file for timezone changes, add logic for parsing config into timezone for a timestamp string

--- jpdb_daily_study_time/lib/timezone.rb
+++ jpdb_daily_study_time/lib/timezone.rb
@@ -1,27 +1,48 @@
 require "tzinfo"
 require "time"
+require "yaml"
 
 module Timezone
+  TimezoneChange = Struct.new(:start_date, :end_date, :timezone, keyword_init: true)
+
+  TIMEZONE_CHANGES_FILEPATH = "config/timzeone_changes.yml"
+  TIMEZONE_CHANGES =
+    if File.exist?(TIMEZONE_CHANGES_FILEPATH)
+      YAML.load(File.read(TIMEZONE_CHANGES_FILEPATH), permitted_classes: [Time])
+        .fetch("timezone_changes", [])
+        .map { |tzc| TimezoneChange.new(start_date: tzc["start_date"], end_date: tzc["end_date"], timezone: tzc["timezone"])}
+    else
+      []
+    end
+  DEFAULT_TIMEZONE = "America/Chicago"
+
   # `timestamp` is a 10-digit integer for seconds since unix epoch
   def self.for_seconds_timestamp(timestamp)
     time_from_timestamp = Time.at(timestamp)
-    timezone_name =
-      if time_from_timestamp > Time.parse("2023-03-02 05:00:00 -0600") &&
-         time_from_timestamp < Time.parse("2023-03-09 05:00:00 -0600")
-        "America/New_York"
-      elsif time_from_timestamp > Time.parse("2023-05-10 05:00:00 -0500") &&
-            time_from_timestamp < Time.parse("2023-05-16 19:00:00 -0500")
-        "America/New_York"
-      elsif time_from_timestamp > Time.parse("2023-06-07 06:00:00 -0500") &&
-            time_from_timestamp < Time.parse("2023-06-10 10:00:00 -0500")
-        "America/Los_Angeles"
-      elsif time_from_timestamp > Time.parse("2023-08-23 05:00:00 -0500") &&
-            time_from_timestamp < Time.parse("2023-08-30 06:00:00 -0500")
-        "America/Los_Angeles"
-      else
-        "America/Chicago"
-      end
+    sorted_timezone_changes = TIMEZONE_CHANGES.sort_by(&:start_date)
+    uncompleted_timezone_changes, completed_timezone_changes = sorted_timezone_changes.partition { |tzc| tzc.end_date.nil? }
 
-    TZInfo::Timezone.get(timezone_name)
+    timezone_name = nil
+    completed_timezone_changes.each do |timezone_change|
+      if time_from_timestamp > timezone_change.start_date && time_from_timestamp < timezone_change.end_date
+        # timestamp falls in the middle of a completed timezone_change
+        timezone_name = timezone_change.timezone
+        break
+      end
+    end
+
+    # if timezone_name is still nil, that means it does not fall in the middle of a completed timezone change
+    # so we want to try and see if there is an uncompleted timezone change that might apply
+    if timezone_name.nil?
+      uncompleted_timezone_changes.each do |timezone_change|
+        if time_from_timestamp > timezone_change.start_date
+          # timestamp falls inside an uncompleted / currently active timezone_change
+          timezone_name = timezone_change.timezone
+          break
+        end
+      end
+    end
+
+    TZInfo::Timezone.get(timezone_name || DEFAULT_TIMEZONE) # use the default if we still haven't found a matching timezone in the config
   end
 end
