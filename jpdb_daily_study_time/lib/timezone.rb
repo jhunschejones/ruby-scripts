ab5154ed41e6f63f9813809d0fc35fd9305963e6
Joshua Hunsche Jones <jhunschejones@github.com>
2023-09-01 10:51:40 -0500

    Add most recent trip and change time parsing to be easier to read

--- jpdb_daily_study_time/lib/timezone.rb
+++ jpdb_daily_study_time/lib/timezone.rb
@@ -1,16 +1,22 @@
 require "tzinfo"
+require "time"
 
 module Timezone
+  # `timestamp` is a 10-digit integer for seconds since unix epoch
   def self.for_seconds_timestamp(timestamp)
+    time_from_timestamp = Time.at(timestamp)
     timezone_name =
-      # March 2 to March 9, 2023 trip to the east coast
-      if timestamp > 1677754800 && timestamp < 1678359600
+      if time_from_timestamp > Time.parse("2023-03-02 05:00:00 -0600") &&
+         time_from_timestamp < Time.parse("2023-03-09 05:00:00 -0600")
         "America/New_York"
-      # May 10 to May 16, 2023 trip to the east coast
-      elsif timestamp > 1683712800 && timestamp < 1684281600
+      elsif time_from_timestamp > Time.parse("2023-05-10 05:00:00 -0500") &&
+            time_from_timestamp < Time.parse("2023-05-16 19:00:00 -0500")
         "America/New_York"
-      # June 7 to June 16, 2023 trip to west coast
-      elsif timestamp > 1686135600 && timestamp < 1686409200
+      elsif time_from_timestamp > Time.parse("2023-06-07 06:00:00 -0500") &&
+            time_from_timestamp < Time.parse("2023-06-10 10:00:00 -0500")
+        "America/Los_Angeles"
+      elsif time_from_timestamp > Time.parse("2023-08-23 05:00:00 -0500") &&
+            time_from_timestamp < Time.parse("2023-08-30 06:00:00 -0500")
         "America/Los_Angeles"
       else
         "America/Chicago"
